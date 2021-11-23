33a16d22e2723f2615433ac0c124fcb1d252114c
Ryan Gerome Regis <ryangeromeregis@gmail.com>
2021-11-24 00:55:41 +0800

    Added ReWORX Text

--- src/sections/Section1.js
+++ src/sections/Section1.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Section1() {
     return (
         <div>
-            
+            <div>
+                <h1>ReWORX</h1>
+            </div>
         </div>
     )
 }
