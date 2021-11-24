9f8e85cc572e08c172361aef21bdcf1f1ac24be1
Ryan Gerome Regis <ryangeromeregis@gmail.com>
2021-11-24 09:24:02 +0800

    Combined Section2a and Section2b into Section2

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import './App.css';
 
 import Header from './sections/components/Header';
 import Section1 from './sections/Section1';
-import Section2a from './sections/Section2a';
-import Section2b from './sections/Section2b';
+import Section2 from './sections/Section2';
 import Section3 from './sections/Section3';
 import Section4 from './sections/Section4';
 import Section5 from './sections/Section5';
 import Section6 from './sections/Section6';
 
 function App() {
-  //TODO Ryan: Header, Section2a, Section2b, Section5
+  //TODO Ryan: Header, Section2(Section2a, Section2b), Section5
   //TODO Ron: Section4, Section6
   //TODO Ralph: Section1, Section3
   return (
-    <div>
+    <div className="App">
       <Header /> 
       <Section1 />
-      <Section2a />
-      <Section2b />
+      <Section2 />
       <Section3 />
       <Section4 />
       <Section5 />
---
+++ src/sections/Section2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Section2a from './Section2a';
+import Section2b from './Section2b';
+function Section2() {
+    return (
+        <div>
+            <Section2a />
+            <Section2b />
+        </div>
+    )
+}
+
+export default Section2
