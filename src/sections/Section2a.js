8d81a3100a9539b5701d654e399b143b7572a89e
Ryan Gerome Regis <ryangeromeregis@gmail.com>
2021-11-28 21:33:08 +0800

    Made black drawer background important

--- src/sections/Section2a.js
+++ src/sections/Section2a.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
+
+
 
 function Section2a() {
+
+
+
     return (
         <div className="section-2-children section-2a">
             
